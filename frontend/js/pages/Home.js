0632b7e9fb37ad245c85e4ec688ed1c900bb21fa
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2019-11-21 00:35:54 -0500

    WIP: basic sidebar using material-ui, need to check and compare with ant-design

--- frontend/js/pages/Home.js
+++ frontend/js/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import SearchBar from '../components/SearchBar/SearchBar';
+import SideBar from '../components/SideBar/SideBar';
 
 const title = 'MyAnimeList score progression over time';
 
@@ -10,6 +11,7 @@ const Home = () => {
   return (
     <>
       <SearchBar className="main-container" />
+      <SideBar />
     </>
   );
 };
