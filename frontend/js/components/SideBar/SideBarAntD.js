fe5e0b6585dbf7afb5fa8061c085e246230fa5e6
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2019-11-23 13:18:19 -0500

    WIP: sidebar exploration using ant design

---
+++ frontend/js/components/SideBar/SideBarAntD.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Layout, Menu, Icon } from 'antd';
+
+import 'antd/dist/antd.css';
+import './SideBarAntDStyle.css';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+export default function PermanentSiderLeft() {
+  return (
+    <Layout>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+        }}
+      >
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+          <Menu.Item key="1">
+            <Icon type="bar-chart" />
+            <span className="nav-text">Search</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout style={{ marginLeft: 200 }}>
+        <Header style={{ background: '#fff', padding: 0 }} />
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>TEST</div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
+  );
+}
--- frontend/js/pages/Home.js
+++ frontend/js/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import SearchBar from '../components/SearchBar/SearchBar';
 import SideBar from '../components/SideBar/SideBar';
+import SideBarAntD from '../components/SideBar/SideBarAntD';
 
 const title = 'MyAnimeList score progression over time';
 
@@ -10,8 +11,8 @@ const Home = () => {
 
   return (
     <>
-      <SearchBar className="main-container" />
-      <SideBar />
+      {/* <SearchBar className="main-container" /> */}
+      <SideBarAntD />
     </>
   );
 };
