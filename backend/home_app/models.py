2523e5406a8f7bd739da99dcc26cfdcef4313260
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2019-11-27 01:24:43 -0500

    WIP: arch db schemas

--- backend/home_app/models.py
+++ backend/home_app/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 from django.db import models  # noqa
 
 
-# Create your models here.
+class Anime(models.Model):
+    mal_id = models.IntegerField(primary_key=True)
+    url = models.URLField()
+
+
+class Score(models.Model):
+    score = models.FloatField()
+    date_time = models.DateTimeField()
+    # foreign key vs many to many
