3b4e4056827a3438a18d92bf03545755355c3db1
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2019-11-28 01:05:00 -0500

    WIP: initial DB schema, makemigrations, not applied yet

--- backend/MALTrendsWeb/settings/base.py
+++ backend/MALTrendsWeb/settings/base.py
@@ -26,6 +26,7 @@ AUTH_USER_MODEL = 'users.User'
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    'home_app.apps.HomeAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/home_app/models.py
+++ backend/home_app/models.py
@@ -5,10 +5,19 @@ from django.db import models  # noqa
 
 class Anime(models.Model):
     mal_id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=300)
+    title_english = models.CharField(max_length=300, blank=True)
+    airing = models.BooleanField(default=False)
     url = models.URLField()
+
+    def __str__(self):
+        return self.title
 
 
 class Score(models.Model):
     score = models.FloatField()
     date_time = models.DateTimeField()
-    # foreign key vs many to many
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.score)
