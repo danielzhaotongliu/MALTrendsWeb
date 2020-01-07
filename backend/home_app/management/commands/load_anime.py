0f95d7d581265b5d52cf840c8a3c51ac54fad105
danielzhaotongliu <ztliu@edu.uwaterloo.ca>
2020-01-06 21:39:26 -0500

    loader: get_or_create anime, WIP: need to bulk_update scores for efficiency

--- backend/home_app/management/commands/load_anime.py
+++ backend/home_app/management/commands/load_anime.py
@@ -1,10 +1,14 @@
 from django.core.management.base import BaseCommand
 from datetime import datetime
+import time
 import json
 import glob
 import sys
 import os
 import re
+import requests
+
+from home_app.models import Anime, Score
 
 
 class Command(BaseCommand):
@@ -17,11 +21,30 @@ class Command(BaseCommand):
         # loop through the snapshots for each anime
         for file_name in glob.glob(data_path):
             file_list = re.findall(r'snapshots_[0-9]+\.jl', file_name)
+            # sanity check
             assert len(file_list) == 1, 'ERROR: invalid path conventions'
             mal_id = int(re.findall(r'\d+', file_list[0])[0])
-            self.stdout.write(f'{mal_id}')
-        #     with open(file_name, 'r') as f:
-        #         for line in f:
-        #             snapshot = json.loads(line)
-        #             time = datetime.utcfromtimestamp(snapshot['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
-        #             score = snapshot['score']
+
+            anime, created = Anime.objects.get_or_create(mal_id=mal_id)
+            if created:
+                # call MAL (unofficial) API endpoint to get anime info
+                r = requests.get(f'https://api.jikan.moe/v3/anime/{mal_id}')
+                r_data = r.json()
+                # update the fields of the specified anime object
+                anime.title = r_data['title']
+                anime.title_english = r_data['title_english']
+                anime.airing = r_data['airing']
+                anime.url = r_data['url']
+                anime.save()
+                # add delay to not overload API
+                time.sleep(5)
+                self.stdout.write(f'Created new anime: "{anime}" with mal_id: {mal_id}')
+            else:
+                self.stdout.write(f'Retrieved existing anime: "{anime}" with mal_id: {mal_id}')
+            
+            # with open(file_name, 'r') as f:
+            #     for line in f:
+            #         snapshot = json.loads(line)
+            #         time = datetime.utcfromtimestamp(snapshot['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
+            #         score = snapshot['score']
+            #         members = snapshot['score_count']
